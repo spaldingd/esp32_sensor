8273b3c0c9e7a07b12586dc125f005abbe51ef49
Daniel <spaldingd@users.noreply.github.com>
2022-06-21 21:57:46 +0100

    updated comments in env_vars.

--- sensor/env_vars.sample.py
+++ sensor/env_vars.sample.py
@@ -2,7 +2,7 @@ ssid = 'ssid'
 password = 'wifipassword'
 topic_pub = b'temp_humid'
 
-# IP address of Raspberry Pi, working as MQTT broker
+# MQTT settings and credentials
 mqtt_server = '192.168.1.1'
 mqtt_port = 1883
 mqtt_user = "mqttuser"
