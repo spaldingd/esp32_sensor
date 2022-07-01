01fe8874daf21d4ebb29e4963b6a701aeb2c2b43
Daniel <spaldingd@users.noreply.github.com>
2022-07-01 22:14:40 +0100

    Saving a copy of readings to local file.

--- sensor/main.py
+++ sensor/main.py
@@ -75,6 +75,9 @@ while True:
                         'humidity_limit_min': round(humidity_limit_min,2),
                         'humidity_limit_max': round(humidity_limit_max,2)}
         print(json_message)
+        with open("data_log.txt",'a') as file:
+            file.write(str(json_message))
+        
         # message = ("{0:10}, {1:8}, {2}, {3:3.2f}, {4:3.2f}".format(datestamp, timestamp, CLIENT_ID, measured_temperature, measured_humidity))
         wifi_connection = connect_wifi()
         # mqtt_client = connect_mqtt()
